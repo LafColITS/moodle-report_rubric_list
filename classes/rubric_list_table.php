ee241b2689e0966af32da4b3014a9831bd548848
Charles Fulton <fultonc@lafayette.edu>
2024-01-09 20:47:17 -0500

    Initial commit

---
+++ classes/rubric_list_table.php
@@ -0,0 +1,70 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Build rubric table.
+ *
+ * @package   report_rubric_list
+ * @copyright 2024 Lafayette College ITS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace report_rubric_list;
+
+class table extends \table_sql {
+
+    /**
+     * Constructor
+     * @param int $uniqueid all tables have to have a unique id, this is used
+     *      as a key when storing table properties like sort order in the session.
+     */
+    function __construct($uniqueid) {
+        parent::__construct($uniqueid);
+        // Define the list of columns to show.
+        $columns = array('id', 'name', 'course', 'copiedfrom');
+        $this->define_columns($columns);
+
+        // Define the titles of columns to show in header.
+        $headers = array('ID', 'Name', 'Course', 'Copied from');
+        $this->define_headers($headers);
+    }
+
+    /**
+     * This function is called for each data row to allow processing of the
+     * username value.
+     *
+     * @param object $values Contains object with all the values of record.
+     * @return $string Return username with link to profile or username only
+     *     when downloading.
+     */
+    function col_name($values) {
+        // If the data is being downloaded than we don't want to show HTML.
+        if ($this->is_downloading()) {
+            return $values->name;
+        } else {
+            return '<a href="/grade/grading/manage.php?areaid='.$values->areaid.'">'.$values->name.'</a>';
+        }
+    }
+
+    function col_course($values) {
+        // If the data is being downloaded than we don't want to show HTML.
+        if ($this->is_downloading()) {
+            return $values->fullname;
+        } else {
+            return '<a href="/course/view.php?id='.$values->courseid.'">'.$values->fullname.'</a>';
+        }        
+    }
+}
---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Display an instructor's rubrics.
+ *
+ * @package   report_rubric_list
+ * @copyright 2024 Lafayette College ITS
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname(__FILE__) . '/../../config.php');
+
+$id = required_param('id', PARAM_INT);
+$course = $DB->get_record('course', array('id' => $id), '*', MUST_EXIST);
+$download = optional_param('download', '', PARAM_ALPHA);
+
+require_login($course);
+
+// Check permissions.
+$coursecontext = context_course::instance($course->id);
+require_capability('report/rubric_list:view', $coursecontext);
+
+// Get the rubrics.
+$table = new report_rubric_list\table($USER->id);
+$table->is_downloading($download);
+
+if (!$table->is_downloading()) {
+    $PAGE->set_title($course->shortname .': '. get_config('report_rubric_list' , 'displayname'));
+    $PAGE->set_heading($course->fullname);
+    echo $OUTPUT->header();
+}
+
+$table->set_sql(
+    'gd.id, gd.areaid, gd.name, gd.copiedfromid, c.id as courseid, c.fullname',
+    '{grading_definitions} gd JOIN {grading_areas} ga ON (gd.areaid =ga.id)
+    JOIN {context} cx ON (ga.contextid = cx.id) JOIN {course_modules} cm ON cx.instanceid = cm.id
+    JOIN {assign} a ON cm.instance = a.id JOIN {course} c ON a.course = c.id',
+    "gd.usercreated = '?'",
+    [$USER->id]
+);
+$table->out(40, true);
+
+if (!$table->is_downloading()) {
+    echo $OUTPUT->footer();
+}
